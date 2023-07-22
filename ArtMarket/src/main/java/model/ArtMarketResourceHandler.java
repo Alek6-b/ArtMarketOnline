3273a0548a9f067cd317626cc8cac0a601ab3bd3
Fabiana0305 <f.cilento1@studenti.unisa.it>
2023-07-22 18:09:52 +0200

    second commit
    prova2

--- ArtMarket/src/main/java/model/ArtMarketResourceHandler.java
+++ ArtMarket/src/main/java/model/ArtMarketResourceHandler.java
@@ -1,11 +1,12 @@
 package model;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ArtMarketResourceHandler {
 	Connection con;
 	ArtMarketResourceHandler() throws SQLException{
-		con = new DataSourceHandler("jdbc:mysql://localhost:3306/artmarketonline", "ArtMarketOnlineManager", "Prodotti").getConnection();
+		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/artmarketonline", "ArtMarketOnlineManager", "Prodotti");
 	}
 }
